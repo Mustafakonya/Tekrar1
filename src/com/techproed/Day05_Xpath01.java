a9eaf7c005d0afceb8b2fcf9adfd08564304eef9
Mustafakonya <av.mustafakonya@gmail.com>
2020-08-22 08:14:47 +0200

    day 5 ders sonu

---
+++ src/com/techproed/Day05_Xpath01.java
@@ -0,0 +1,30 @@
+package com.techproed;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Day05_Xpath01 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/admin/Documents/selenium libraries/drivers/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http:/a.testaddressbook.com/sign_in");
+
+        WebElement email = driver.findElement(By.xpath("//input[@type='email']"));
+        email.sendKeys("testtechproed@gmail.com");
+
+        WebElement sifre = driver.findElement(By.xpath("//input[@type='password']"));
+        sifre.sendKeys("Test1234!");
+
+        WebElement sign = driver.findElement(By.xpath("//input[@name='commit']"));
+        sign.click();
+
+    }
+}
